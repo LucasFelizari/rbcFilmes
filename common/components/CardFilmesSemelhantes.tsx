9dbc70f932a993c99c26dc2d129a83dbf165e83a
Lucas <lucasfelizari@gmail.com>
2023-05-09 20:09:54 -0300

    deixando o card bonitao

--- common/components/CardFilmesSemelhantes.tsx
+++ common/components/CardFilmesSemelhantes.tsx
@@ -1,8 +1,7 @@
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Center, Heading, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Box, Card, CardBody, CardHeader, Center, Heading, ScaleFade, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { IFilmeDto } from "../types/IFilmeDto";
 import { useEffect, useState } from "react";
 import buscarFilmesSemelhantes from "../services/buscarFilmesSemelhantes";
-import Image from "next/image";
 import ImagemFilme from "./ImagemFilme";
 
 export default function CardFilmesSemelhantes({ filmeBuscado }: { filmeBuscado: IFilmeDto }) {
@@ -21,23 +20,25 @@ export default function CardFilmesSemelhantes({ filmeBuscado }: { filmeBuscado: 
 
   if (filmesSemelhantes && filmesSemelhantes.length > 0) {
     return (
-      <Box color={'white'} w={'100%'} borderRadius={'1rem'} borderWidth={'2px'}>
-        <Heading size={'md'} p={'1rem'}>Filmes semelhantes:</Heading>
+      <Box color={'white'} w={'100%'} >
+        <Heading p={'1rem'}>Filmes semelhantes:</Heading>
         <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(300px, 1fr))' p="1rem">
           {filmesSemelhantes.map((filme, index) => (
-            <Card key={index} bg="gray.800" color={'white'}>
-              <CardHeader>
-                <Heading size='md'>{filme.title}</Heading>
-              </CardHeader>
-              <CardBody>
-                <VStack w={'100%'} spacing={'1rem'}>
-                  <Center>
-                    <ImagemFilme maxH={'15rem'} nomeFilme={filme.title} />
-                  </Center>
-                  <Text>Data de lançamento: {filme.release_date}</Text>
-                </VStack>
-              </CardBody>
-            </Card>
+            <ScaleFade initialScale={0.4} in={true} >
+              <Card key={index} bg="gray.800" color={'white'} borderRadius={'1rem'} boxShadow={'dark-lg'}>
+                <CardHeader>
+                  <Heading size='md'>{filme.title}</Heading>
+                </CardHeader>
+                <CardBody>
+                  <VStack w={'100%'} spacing={'1rem'}>
+                    <Center>
+                      <ImagemFilme maxH={'15rem'} nomeFilme={filme.title} />
+                    </Center>
+                    <Text>Data de lançamento: {filme.release_date}</Text>
+                  </VStack>
+                </CardBody>
+              </Card>
+            </ScaleFade>
           ))}
         </SimpleGrid>
 
